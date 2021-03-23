ce2281aba63c7155ce90e50e934ed5ab19b686c8
Vicki Keeley <vicki.keeley@gmail.com>
2021-03-23 21:16:00 +0000

    add firebase and associated config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import { createApp } from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDubRrqAIFIPxbNQ1CJfQ5Ow2kULHcVR50',
+  authDomain: 'shopping-cart-8606f.firebaseapp.com',
+  projectId: 'shopping-cart-8606f',
+  storageBucket: 'shopping-cart-8606f.appspot.com',
+  messagingSenderId: '445960438626',
+  appId: '1:445960438626:web:29d47dd552ffa93647623b'
+}
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
+
+console.log(db)
 
 createApp(App).use(store).use(router).mount('#app')
