5018eed95e39d6612d4de01ff52150a2dac8839d
Vicki Keeley <vicki.keeley@gmail.com>
2021-03-29 18:31:14 +0100

    wip cart markup

---
+++ src/components/CartItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="cart-item">
+    {{ item.name }}
+    <dl>
+      <dt>Price</dt>
+      <dd>{{ item.price }}</dd>
+      <dt>Quantity</dt>
+      <dd>{{ item.quantity }}</dd>
+      <dt>Cost</dt>
+      <dd>{{ item.price * item.quantity }}</dd>
+      <dt>Remove</dt>
+      <dd>
+        <button class="cart-item__remove">Remove</button>
+      </dd>
+    </dl>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CartItem',
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  setup () {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,12 +1,32 @@
 <template>
   <div class="cart">
-    You have nothing in your cart currently. <router-link to="/">Go to Products</router-link>?
+    <p v-if="cart.lenth < 1" class="cart__empty">You have nothing in your cart currently. <router-link to="/">Go to Products</router-link>?</p>
+    <ul class="cart__list">
+      <li v-for="(item, i) in cart" :key="i">
+        <cart-item :item="item"></cart-item>
+      </li>
+    </ul>
+    <button class="cart__checkout">Checkout</button>
   </div>
 </template>
 
 <script>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+import CartItem from '@/components/CartItem.vue'
 
 export default {
-  name: 'Cart'
+  name: 'Cart',
+  components: {
+    CartItem
+  },
+  setup () {
+    const store = useStore()
+    const cart = computed(() => store.state.cart.cart)
+
+    return {
+      cart
+    }
+  }
 }
 </script>
